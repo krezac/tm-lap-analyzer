868d2a9e184a2afa303478d66609f6a4368f785e
Kamil Rezac <krezac@gmail.com>
2019-11-26 07:34:23 +0000

    basic server (pg stuff not called)

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,15 @@
 FROM python:3.8.0-alpine3.10
 
+EXPOSE 8000
+ADD src /src
+
+RUN apk add --no-cache --virtual .build-deps \
+    gcc \
+    python3-dev \
+    musl-dev \
+    postgresql-dev \
+    && pip install --no-cache-dir psycopg2 numpy \
+    && apk del --no-cache .build-deps
+
+CMD [ "python", "./src/server.py" ]
+
---
+++ src/server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+ 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+import psycopg2
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+ 
+def run():
+  print('starting server...')
+ 
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('', 8000)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+
+def do_db_stuff():
+    conn = None
+    try:
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost",database="suppliers", user="postgres", password="postgres")
+        cur = conn.cursor()
+        cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+ 
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+ 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+ 
+run()
